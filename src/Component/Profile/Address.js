9a86c78b75a06714b35245296ddc3c1e933e9f7f
MarouenAmor <marouen.amor.89@gmail.com>
2021-10-28 10:16:26 +0100

    reactcomponent

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <h3>rue d algerie la gare Gsar Gafsa </h3>
+    </div>
+  );
+};
+
+export default Address;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const profilePhoto = () => {
+    return (
+        <div>
+       <img src="/gmc.png" alt="GoMyCode"/>     
+        </div>
+    );
+};
+
+export default profilePhoto ;
